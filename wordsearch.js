44e89dc99644f4cee60b7793c5f4623c0c9b0ec1
NHadjiraIA <bkh.hadjira@gmail.com>
2021-07-18 03:51:03 -0400

    apdate wordsearch.js

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,29 @@
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
+     console.log(horizontalJoin)
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+        if (l.includes(word)){
+  
+        return true;
+        } else{
+          let k =0;
+          let wordExist = "";
+          let arrayWord = [];
+          for (let i = 0;  i < letters.length; i++) {
+            for ( let j =0; j <letters[i].length; j++) {           
+            wordExist = wordExist + letters[i][k];            
+            }
+            k=k+1;
+          }
+          arrayWord.push(word)
+          for (l of arrayWord) {
+            if (l.includes(word)){
+              return true;
+            }
+          return false; 
+          }
+        }
+      }
+  }
 
 module.exports = wordSearch
